84e5493e7b7b6e6a8185cdab2ff8b31401d81c43
Rafael Campos <rafaelccamposdev@gmail.com>
2020-08-17 01:45:34 -0300

    Configuração do ESlint, adicionando prop-types, criação e estilziação do formulário cad-categorias.

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria () {
+    const [categorias, setCategorias] = useState([]);
+
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    };
+
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue (key, value){
+        // key: nome, descricao, cor
+        setValues({
+            ...values,
+            [key]: value, //nome:'nome categoria'
+        })
+    };
+
+    function handleChange(infosDoEvento) {
+        //const { getAttribute, value } = infosDoEvento.target;
+        setValue(
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+            );
+        };
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
+
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
+                setValues(valoresIniciais);
+            }}>
+               
+                <FormField
+                    label="Categoria"
+                    type="text"
+                    value={values.nome}
+                    name="nome"
+                    onChange={handleChange}
+                />
+                
+                <FormField
+                    label="Descrição"
+                    type="textarea"
+                    value={values.descricao}
+                    name="descricao"
+                    onChange={handleChange}
+                />
+
+                <FormField
+                    label="Cor"
+                    type="color"
+                    value={values.cor}
+                    name="cor"
+                    onChange={handleChange}
+                />
+
+                <Button>
+                    Cadastrar
+                </Button>
+            </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria.nome}${indice}`}>
+                            {categoria.nome}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para Home
             </Link>
         </PageDefault>
     )
-}
+};
+
+
+
+
+
+
+
+
 
 export default CadastroCategoria;
